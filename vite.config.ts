5fb9714e07f182b494324b880ca6043ff451eb3f
weishaodaren <weishaodaren@gmail.com>
2021-03-26 14:59:53 +0800

    fix resolve.alias

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,15 @@ import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  alias: {
-    '@': resolve('src'),
-    Assets: resolve('src/assets'),
-    Components: resolve('src/components'),
-    Hooks: resolve('src/hooks'),
-    Utils: resolve('src/utils'),
-    Pages: resolve('src/pages'),
+  resolve: {
+    alias: {
+      '@': resolve('src'),
+      Assets: resolve('src/assets'),
+      Components: resolve('src/components'),
+      Hooks: resolve('src/hooks'),
+      Utils: resolve('src/utils'),
+      Pages: resolve('src/pages'),
+    },
   },
   plugins: [vue()],
 });
