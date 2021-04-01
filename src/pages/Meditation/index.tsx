065d8fabcbd79b8718d0749eb39b6e83498d2970
weishaodaren <weishaodaren@gmail.com>
2021-04-01 13:34:51 +0800

    useFetch 转移 useStore

--- src/pages/Meditation/index.tsx
+++ src/pages/Meditation/index.tsx
@@ -1,49 +1,48 @@
 import {
+  computed,
   defineComponent,
   onBeforeMount,
   onMounted,
   reactive,
   ref,
-  watchEffect,
+  unref,
 } from 'vue';
-import { useFetch } from 'Hooks';
-import { userType } from '@/types';
+import { useStore } from 'vuex';
+import { key } from '@/stores';
 import { Container, Avatar, Describe, Name, NickName, Profile } from './styles';
 
 const Meditation = defineComponent({
   setup() {
+    const $store = useStore(key);
     const url = ref('users/weishaodaren');
-    const userData = reactive({
-      data: {} as userType.UserData,
-    });
 
-    onBeforeMount(async () => {
-      const { data, loading, run: runFetch } = useFetch({
-        url,
+    onBeforeMount(() =>
+      $store.dispatch('getUserData', {
+        url: unref(url),
         method: 'GET',
         immediate: false,
-      });
-      await runFetch();
-      userData.data = data.value;
-    });
+      })
+    );
 
     onMounted(() => {
       document.body.style.backgroundColor = `#126`;
     });
 
+    const { value: userData } = computed(() => $store.getters.userData);
+
     return () => (
       <Container>
-        <Avatar src={userData.data?.avatar_url as string} />
+        <Avatar src={userData.avatar_url as string} />
         <Describe>
-          <Name>{userData.data?.name}</Name>
-          <NickName>{userData.data?.login}</NickName>
+          <Name>{userData?.name}</Name>
+          <NickName>{userData?.login}</NickName>
         </Describe>
         <Profile>
-          <div>{userData.data?.bio}</div>
+          <div>{userData?.bio}</div>
           <button>Edit profile</button>
           <div>
-            {userData.data?.followers}followers
-            {userData.data?.following}following
+            {userData?.followers} followers
+            {userData?.following}following
           </div>
         </Profile>
       </Container>
--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,28 +1,42 @@
 import { InjectionKey } from 'vue';
 import { createStore, Store } from 'vuex';
+import { userType } from '@/types';
+import { useFetch } from 'Hooks';
 
 export interface IState {
   count: number;
-  [x: string]: string | number;
+  userData: userType.UserData;
 }
 
 export const key: InjectionKey<Store<IState>> = Symbol();
 export const store = createStore<IState>({
   state: {
     count: 0,
+    userData: {},
   },
   getters: {
     count: (state) => state.count,
+    userData: (state) => state.userData,
   },
   mutations: {
     increment(state, payload) {
       if (typeof payload === 'number') state.count += payload;
       else if (typeof payload === 'object') state.count += payload.amount;
     },
+    getUserData(state, payload) {
+      return (state.userData = { ...payload.value });
+    },
   },
   actions: {
     increments({ commit }, payload) {
       commit('increment', payload);
+    },
+    async getUserData(ctx, payload) {
+      const { data, run } = useFetch({
+        ...payload,
+      });
+      await run();
+      ctx.commit('getUserData', data);
     },
   },
   modules: {},
