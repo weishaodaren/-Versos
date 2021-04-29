b35cb01a2bb552eecfcee31c8e19e00847cb156d
weishaodaren <weishaodaren@gmail.com>
2021-04-29 15:24:21 +0800

    update Object

--- src/pages/Beer/index.tsx
+++ src/pages/Beer/index.tsx
@@ -1,7 +1,15 @@
 import { defineComponent, Fragment, ref, computed } from 'vue';
 import { useRouter } from 'vue-router';
 import { WrappedInput, Button } from './style';
-import { constant } from 'lodash';
+import {
+  constant,
+  bindAll,
+  camelCase,
+  capitalize,
+  kebabCase,
+  truncate,
+  at,
+} from 'lodash';
 import { inRange, clamp, uniqueId, times } from '@/utils';
 
 const List = [
@@ -18,7 +26,20 @@ const Beer = defineComponent({
     // const sample = inRange(-10, -19, 10);
     // const sample = random(-20, 20);
     // const sample = uniqueId('weishaodaren');
-    const sample = times(100, constant({ name: 'weishaodaren ' }));
+    const view = {
+      label: 'docs',
+      click: () => console.log(`clicked:${view.label}`),
+      mouseDown: (args: string) => Number(args),
+    };
+    const demo = { a: [{ b: { c: 3 } }, 4] };
+    // const sample = times(100, constant({ name: 'weishaodaren ' }));
+    // const sample = bindAll(view);
+    // const sample = camelCase('_A_B_C_D');
+    // const sample = capitalize('WEISHAODAREN');
+    // const sample = kebabCase('W E I S H A O D A R E N');
+    // const sample = truncate('15821044438', { length: 4 });
+    // const sample = Object.assign(view, { a: 'BB', label: 'xixix' });
+    const sample = at(demo, ['a[0].b.c', 'a[1]']);
     console.log(sample, 'sample');
 
     const placeholder = ref<string>(`input ur GitHub userName...`);
