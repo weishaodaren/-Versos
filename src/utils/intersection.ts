6a6d235634177cbe036abab641f02f53012f682f
weishaodaren <weishaodaren@gmail.com>
2021-05-11 09:41:11 +0800

    update

--- src/utils/intersection.ts
+++ src/utils/intersection.ts
@@ -1,3 +1,23 @@
-const intersection = () => {};
+const intersection: (
+  arrays: Array<string>,
+  iteratee: () => void,
+  comparator: () => void
+) => void = (A, I, C) => {
+  const length = A[0].length;
+  const otherLength = A.length;
+  const caches = new Array(otherLength);
+  const result = [];
+
+  let array: string[] | string;
+  let maxLength = Infinity;
+  let otherIndex = otherLength;
+
+  while (otherIndex--) {
+    array = A[otherIndex];
+    if (otherIndex && I) {
+      // array = (<unknown>array).map((el) => I(el));
+    }
+  }
+};
 
 export default intersection;
